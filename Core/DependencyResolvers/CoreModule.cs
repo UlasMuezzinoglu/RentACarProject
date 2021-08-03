76874bdad788d2b6bf8c01d0c530b8f97d1a7154
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-08-03 18:20:01 +0300

    Configured Jwt

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceCollction)
+        {
+            serviceCollction.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+    }
+}
