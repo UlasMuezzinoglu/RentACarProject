a896702c4c20ea229c2a79dd89b5f42ed1640818
UlasMuezzinoglu <batan_gem1@outlook.com>
2021-08-03 19:55:23 +0300

    Added Caching Model

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,6 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.CrossCuttingConcerns.Caching;
+using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,7 +15,10 @@ namespace Core.DependencyResolvers
     {
         public void Load(IServiceCollection serviceCollction)
         {
+            serviceCollction.AddMemoryCache();
             serviceCollction.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            serviceCollction.AddSingleton<ICacheManager, MemoryCacheManager>();
+
         }
     }
 }
